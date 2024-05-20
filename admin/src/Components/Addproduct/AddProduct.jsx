11302550a1926a3c3285686c8d99321acc2ce8b7
honyrong <honyrong@gmail.com>
2024-05-20 21:32:56 +0300

    admin components

---
+++ admin/src/Components/Addproduct/AddProduct.jsx
@@ -0,0 +1,12 @@
+import React from 'react'; 
+import './AddProduct.css';
+
+const AddProduct = () => {
+    return (
+        <div className="addproduct">
+        
+        </div>
+    )
+}
+
+export default AddProduct
